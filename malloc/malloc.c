419ba2c6964b7278d698aee18dacbde289cecc77
cabbage <cab226023@gmail.com>
2025-07-06 15:57:08 +0900

    implement a buggy versino of free bins, which lack checking bins above the required size and leads to huge memory usage

--- malloc/malloc.c
+++ malloc/malloc.c
@@ -10,10 +10,11 @@
 
 // Interfaces to get memory pages from OS
 
-
 void *mmap_from_system(size_t size);
 void munmap_to_system(void *ptr, size_t size);
 
+//10 bins for 2's power of size
+#define BIN_NUMBER 10
 
 // Struct definitions
 
@@ -23,25 +24,43 @@ typedef struct metadata_t {
   struct metadata_t *prev;
 } metadata_t;
 
-typedef struct my_heap_t {
+typedef struct bin_t {
   metadata_t dummy_head;
   metadata_t dummy_tail; 
-} my_heap_t;
+} bin_t;
 
+typedef struct heap_t {
+  // an array of bins
+  bin_t bins[BIN_NUMBER];
+} heap_t;
 
 // Static variables (DO NOT ADD ANOTHER STATIC VARIABLES!)
-my_heap_t my_heap;
+heap_t my_heap;
 
 
 // Helper functions (feel free to add/remove/edit!)
 
+int get_bin_index(size_t size) {
+  if (size <= 8) return 0;   // bin 0: 0-8 bytes
+  if (size <= 16) return 1;  // bin 1: 8-16 bytes
+  if (size <= 32) return 2;  // bin 2: 16-32 bytes
+  if (size <= 64) return 3;  // bin 3: 32-64 bytes
+  if (size <= 128) return 4; // bin 4: 64-128 bytes
+  if (size <= 256) return 5; // bin 5: 128-256 bytes
+  if (size <= 512) return 6; // bin 6: 256-512 bytes
+  if (size <= 1024) return 7; // bin 7: 512-1024 bytes
+  if (size <= 2048) return 8; // bin 8: 1024-2048 bytes
+  return 9; // bin 9: 2048-4096 bytes (and above)
+}
+
 void my_add_to_free_list(metadata_t *metadata) {
   assert(!metadata->next);
-  metadata->next = my_heap.dummy_head.next;
-  metadata->prev = &my_heap.dummy_head;
-  my_heap.dummy_head.next->prev = metadata;
-  my_heap.dummy_head.next = metadata;
-
+  int bin_idx = get_bin_index(metadata->size);
+  bin_t *bin = &my_heap.bins[bin_idx];
+  metadata->next = bin->dummy_head.next; //the next is a pointer
+  metadata->prev = &bin->dummy_head; // the dummy_head is an actuall value, so need &
+  bin->dummy_head.next->prev = metadata;
+  bin->dummy_head.next = metadata;
 }
 
 void my_remove_from_free_list(metadata_t *metadata) {
@@ -53,18 +72,19 @@ void my_remove_from_free_list(metadata_t *metadata) {
   metadata->prev = NULL;
 }
 
-//
+
 // Interfaces of malloc (DO NOT RENAME FOLLOWING FUNCTIONS!)
-//
 
 // This is called at the beginning of each challenge.
 void my_initialize() {
-  my_heap.dummy_head.size = 0;
-  my_heap.dummy_tail.size = 0;
-  my_heap.dummy_head.next = &my_heap.dummy_tail;
-  my_heap.dummy_tail.prev = &my_heap.dummy_head;
-  my_heap.dummy_tail.next = NULL;
-  my_heap.dummy_head.prev = NULL;
+  for (int i = 0; i < BIN_NUMBER; i++){
+    my_heap.bins[i].dummy_head.size = 0;
+    my_heap.bins[i].dummy_tail.size = 0;
+    my_heap.bins[i].dummy_head.next = &my_heap.bins[i].dummy_tail;
+    my_heap.bins[i].dummy_tail.prev = &my_heap.bins[i].dummy_head;
+    my_heap.bins[i].dummy_tail.next = NULL;
+    my_heap.bins[i].dummy_head.prev = NULL;
+  }
 }
 
 // my_malloc() is called every time an object is allocated.
@@ -72,10 +92,11 @@ void my_initialize() {
 // 4000. You are not allowed to use any library functions other than
 // mmap_from_system() / munmap_to_system().
 void *my_malloc(size_t size) {
-  metadata_t *metadata = my_heap.dummy_head.next;
+  int bin_idx=get_bin_index(size);
+  metadata_t *metadata = my_heap.bins[bin_idx].dummy_head.next;
   metadata_t *best_slot=NULL; // a pointer variable to keep watch the current best fit
 
-  while (metadata != &my_heap.dummy_tail ) {
+  while (metadata != &my_heap.bins[bin_idx].dummy_tail ) {
     if (metadata->size >= size){
       if (!best_slot || best_slot->size > metadata->size){ 
         // update best_slot if found a fitter metadata
@@ -88,17 +109,20 @@ void *my_malloc(size_t size) {
   if (!best_slot) {
     // cannot find free slot available. request a new memory region from the system by calling mmap_from_system().
     // buffer_size: metadata + free_slot
-    //     | metadata | free slot |
-    //     ^
-    //     metadata
-    //     <---------------------->
-    //            buffer_size
     size_t buffer_size = 4096;
     metadata_t *new_metadata = (metadata_t *)mmap_from_system(buffer_size); //request a new block of 4096 
-    new_metadata->size = buffer_size - sizeof(metadata_t);
+    new_metadata->size = size;
     new_metadata->next = NULL;
     new_metadata->prev = NULL;
-    // Add the memory region to the free list.
+
+    size_t new_remain_size = buffer_size - size;
+    if(new_remain_size > sizeof(metadata_t)){
+      void *end_of_new_metadata = new_metadata +1;
+      metadata_t *new_remain_of_new_metadata = (metadata_t *)((char *)end_of_new_metadata + size);
+      // add the remain of new requested space to corresponding free list
+      my_add_to_free_list(new_remain_of_new_metadata);
+    }
+    // Add the memory region to the correct size of free list.
     my_add_to_free_list(new_metadata);
     // try my_malloc() again. 
     return my_malloc(size);
@@ -117,13 +141,7 @@ void *my_malloc(size_t size) {
     // this code path will not be taken and the region will be managed
     // as a part of the allocated object.
     best_slot->size = size; // currently the best_slot represents an allocated space, so it's size is used size
-    // Create a new metadata for the remaining free slot.
-    //
-    // ... | metadata | object | metadata | free slot | ...
-    //     ^          ^        ^
-    //     metadata   ptr      new_metadata
-    //                 <------><---------------------->
-    //                   size       remaining size
+    // Create a new metadata for the remaining free slot that comes after allocated object
     metadata_t *new_metadata = (metadata_t *)((char *)ptr + size);//pointer arithmetic, 
     // cast to metadata_t cuz we put new free slot here
     new_metadata->size = remaining_size - sizeof(metadata_t);
